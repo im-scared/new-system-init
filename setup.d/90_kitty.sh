e0b06585bd5e16a807c2c80130055b0cb91cdf60
ditzy <thesourceofx@gmail.com>
2021-12-19 09:20:28 -0600

    rename shell setup and change it to link the whole kitty folder

---
+++ setup.d/90_kitty.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+header "Configuring kitty"
+ensure-link "$CONFIG_DIR" "$HOME/.config/kitty"
+
--- setup.d/90_shell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-header "Configuring shell"
-ensure-link "$CONFIG_DIR/kitty.conf" "$HOME/.config/kitty/kitty.conf"
-
