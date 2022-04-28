d37d3551082189e73406e4dd0b2b2aeaa6dbb8a2
ditzy <ditzy.zy@pm.me>
2022-04-28 05:23:59 -0500

    add brave browser to package installs

--- setup.d/10s_packages.sh
+++ setup.d/10s_packages.sh
@@ -10,6 +10,7 @@ apt install -y \
   i3blocks \
   dunst \
   compton \
+  apt-transport-https \
   curl \
   fonts-mplus \
   fonts-noto \
@@ -37,6 +38,18 @@ apt install -y \
   xsel \
   xsettingsd \
   zsh
+
+header "Installing Brave browser"
+brave_sources_file=/etc/apt/sources.list.d/brave-browser-release.list
+if [[ -f "$brave_sources_file" ]]; then
+  echo "Brave sources file found"
+else
+  echo "Adding Brave package source..."
+  curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee "$brave_sources_file"
+  apt update
+fi
+apt install -y brave-browser
 
 header "Updating alternatives"
 update-alternatives --set x-terminal-emulator /usr/bin/kitty
