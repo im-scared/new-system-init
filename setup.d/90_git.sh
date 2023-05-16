c8033e186798b3834bfae17679ad9674161a5b64
ditzy <thesourceofx@gmail.com>
2023-05-16 05:39:06 -0500

    set repo to git protocol in setup

--- setup.d/90_git.sh
+++ setup.d/90_git.sh
@@ -4,3 +4,11 @@ header "Configuring git"
 
 git config --global user.email "thesourceofx@gmail.com"
 git config --global user.name "ditzy"
+
+cd "$REPO_DIR"
+if [[ "$(git remote get-url origin)" =~ ^git ]]; then
+  echo "Git repo origin correctly using git protocol."
+else
+  echo "Git repo origin is using https protocol. Switching to git protocol..."
+  git remote set-url origin git@github.com:im-scared/new-system-init.git
+fi
